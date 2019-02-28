d9f806ae0879e1eb5e328383493b87b0692e7ce0
Mihir Prabhudesai <mihirp1998.mp@gmail.com>
2019-02-28 17:18:44 -0500

    check

--- network.py
+++ network.py
@@ -99,12 +99,12 @@ class HyperNetwork(nn.Module):
         #self.linear1 = nn.Linear(32, f, bias=True)
         #self.linear1 = nn.DataParallel(self.linear1)
         #uncomment
-        self.encoderWeights =  nn.ParameterList([Parameter(torch.fmod(torch.randn((emb_dimension, self.total(i) )),2)) for i in self.enclayer])
-        self.decoderWeights =  nn.ParameterList([Parameter(torch.fmod(torch.randn((emb_dimension, self.total(i) )),2)) for i in self.declayer])
-        self.binWeights =  nn.ParameterList([Parameter(torch.fmod(torch.randn((emb_dimension, self.total(i) )),2)) for i in self.binlayer])
-        # self.encoderWeights = nn.ParameterList([Parameter(torch.nn.init.xavier_normal_(torch.empty(emb_dimension, self.total(i)) )) for i in self.enclayer])
-        # self.decoderWeights = nn.ParameterList([Parameter(torch.nn.init.xavier_normal_(torch.empty(emb_dimension, self.total(i)) )) for i in self.declayer])
-        # self.binWeights = nn.ParameterList([Parameter(torch.nn.init.xavier_normal_(torch.empty(emb_dimension, self.total(i)) )) for i in self.binlayer])
+        # self.encoderWeights =  nn.ParameterList([Parameter(torch.fmod(torch.randn((emb_dimension, self.total(i) )),2)) for i in self.enclayer])
+        # self.decoderWeights =  nn.ParameterList([Parameter(torch.fmod(torch.randn((emb_dimension, self.total(i) )),2)) for i in self.declayer])
+        # self.binWeights =  nn.ParameterList([Parameter(torch.fmod(torch.randn((emb_dimension, self.total(i) )),2)) for i in self.binlayer])
+        self.encoderWeights = nn.ParameterList([Parameter(torch.nn.init.xavier_normal_(torch.empty(emb_dimension, self.total(i)) )) for i in self.enclayer])
+        self.decoderWeights = nn.ParameterList([Parameter(torch.nn.init.xavier_normal_(torch.empty(emb_dimension, self.total(i)) )) for i in self.declayer])
+        self.binWeights = nn.ParameterList([Parameter(torch.nn.init.xavier_normal_(torch.empty(emb_dimension, self.total(i)) )) for i in self.binlayer])
 
         #self.w1 =torch.nn.init.xavier_normal(self.w1)
         self.encoderBias = nn.ParameterList([Parameter(torch.fmod(torch.zeros((self.total(i))),2)) for i in self.enclayer])
